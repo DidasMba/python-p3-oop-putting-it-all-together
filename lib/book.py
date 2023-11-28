ccee5c95eb79961bd29b84556f96339e7023108e
DidasMba <daniel.mbarushi@gmail.com>
2023-11-28 13:40:44 +0300

    oopputting

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
+# book.py
+
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+# shoe.py
+
