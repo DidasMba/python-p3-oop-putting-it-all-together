00dddc587a1bfba2a64700db656568532579c215
DidasMba <daniel.mbarushi@gmail.com>
2023-11-28 14:26:01 +0300

    succespush

--- lib/shoe.py
+++ lib/shoe.py
@@ -2,10 +2,32 @@
 
 class Shoe:
     def __init__(self, brand, size):
-        self.brand = brand
-        self.size = size
-        self.condition = "New"
+        self._brand = brand
+        self._size = size
+        self._condition = "New"
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, new_size):
+        if not isinstance(new_size, int):
+            print("size must be an integer")
+            return
+        self._size = new_size
+
+    @property
+    def condition(self):
+        return self._condition
 
     def cobble(self):
-        print("The shoe has been repaired.")
-        self.condition = "New"
+        print("Your shoe is as good as new!")
+        self._condition = "New"
+
+
+
